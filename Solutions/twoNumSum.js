e0f2141566a3abc5df1ed032f6dda17a897c419a
pairofAces <karanchauhan16@gmail.com>
2021-01-09 22:16:16 -0500

    started the 2 for loop iterations

--- Solutions/twoNumSum.js
+++ Solutions/twoNumSum.js
@@ -6,5 +6,10 @@ const twoNumSum = (array, targetSum) => {
     const result = [];
     let min, max;
 
-    
+    // using for loops
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            
+        }
+    }
 }
