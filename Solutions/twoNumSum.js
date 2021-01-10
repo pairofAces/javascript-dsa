e40035bc8ae9d4dc14b2af4db794ab6a41a621e7
pairofAces <karanchauhan16@gmail.com>
2021-01-09 22:19:39 -0500

    generated solution for twoNumSum -> 2 for loops, better way with hash

--- Solutions/twoNumSum.js
+++ Solutions/twoNumSum.js
@@ -9,7 +9,14 @@ const twoNumSum = (array, targetSum) => {
     // using for loops
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length; j++) {
-            
+            let a = array[i];
+            let b = array[j];
+            if (a + b === targetSum) {
+                min = Math.min(a, b);
+                max = Math.max(a, b);
+                return [min, max];
+            }
         }
     }
+    return result;
 }
