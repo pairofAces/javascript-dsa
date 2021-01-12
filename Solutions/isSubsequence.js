cd4ca4998cf3ba1b01cb973edfb612c2da859d57
pairofAces <karanchauhan16@gmail.com>
2021-01-11 22:10:30 -0500

    finished explanation for leetcode 392

--- Solutions/isSubsequence.js
+++ Solutions/isSubsequence.js
@@ -9,6 +9,8 @@ const isSubsequence = (s ,t) => {
     // make a while loop that will execute when the indices 
     // are less than the length of the strings
     while (sIndex < s.length && tIndex < t.length) {
+        // use an if/else statement to check if the letters that
+        // are in the indices of each string, are equal
         if (t[tIndex] === s[sIndex]) {
             sIndex += 1;
             tIndex += 1;
@@ -16,5 +18,8 @@ const isSubsequence = (s ,t) => {
             tIndex += 1;
         };
     };
+    // upon successful iteration, return the boolean of (sIndex)
+    // and (s.length) -> should be equal, returning a 
+    // 'true' or 'false'
     return sIndex === s.length;
 };
