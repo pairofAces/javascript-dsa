6aa3194b64ec67887fe1a185f442689dfc38d7af
pairofAces <karanchauhan16@gmail.com>
2021-01-11 22:06:59 -0500

    creating explanation

--- Solutions/isSubsequence.js
+++ Solutions/isSubsequence.js
@@ -3,8 +3,11 @@
 // of (t).
 
 const isSubsequence = (s ,t) => {
+    // create variables for the index of the strings
     let sIndex = 0;
     let tIndex = 0;
+    // make a while loop that will execute when the indices 
+    // are less than the length of the strings
     while (sIndex < s.length && tIndex < t.length) {
         if (t[tIndex] === s[sIndex]) {
             sIndex += 1;
