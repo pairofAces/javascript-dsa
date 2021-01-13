509129df11b45bf6c15e691324fae701fc1d47f7
pairofAces <karanchauhan16@gmail.com>
2021-01-12 22:48:13 -0500

    added closest value in BST, question

---
+++ Solutions/findClosestValueInBst.js
@@ -0,0 +1,7 @@
+// A.E. solution
+
+// write a function that takes in a Binary Search Tree (BST) and a
+// target integer value and returns the closest value to that target
+// value contained in the BST
+
+// assumption: there can only be one closest value
