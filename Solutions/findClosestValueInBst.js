c9e063b1a241d6dee32552c264e68a6806cc7d6a
pairofAces <karanchauhan16@gmail.com>
2021-01-12 22:50:40 -0500

    added solution template for closest value in BST

--- Solutions/findClosestValueInBst.js
+++ Solutions/findClosestValueInBst.js
@@ -5,3 +5,15 @@
 // value contained in the BST
 
 // assumption: there can only be one closest value
+
+function findClosestValueInBst(tree, target) {
+    // I'll make a helper function outside of this scope
+    // then envoke it here
+    // (aspirational code)
+    return closestVaueBstHelper(tree, target, tree.value);
+};
+
+// create the helper function here
+function closestVaueBstHelper(tree, target, closest) {
+
+};
