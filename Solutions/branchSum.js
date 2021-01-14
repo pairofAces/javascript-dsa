ef12bdc0ce06a68f6a243d5fef2d38b7662e7a13
pairofAces <karanchauhan16@gmail.com>
2021-01-13 23:04:06 -0500

    added explanation for branchSum.js

--- Solutions/branchSum.js
+++ Solutions/branchSum.js
@@ -3,3 +3,7 @@
 // Write a function that takes in a Binary tree and returns a list
 // of its branch sums ordered from leftmost branch sum to rightmost 
 // branch sum
+
+// A branch sum is a the sum of all the values in a Binary Tree Branch. 
+    // A Binary Tree Branch is a a path of nodes in a tree that starts 
+    // at the (root) node and ends at any (leaf) node. 
