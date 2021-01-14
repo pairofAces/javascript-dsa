be812d384fe0bc325733fdc6289453dbf0414434
pairofAces <karanchauhan16@gmail.com>
2021-01-13 23:01:41 -0500

    adding question template- branchSum.js

---
+++ Solutions/branchSum.js
@@ -0,0 +1,5 @@
+// Branch Sums - A.E. solution
+
+// Write a function that takes in a Binary tree and returns a list
+// of its branch sums ordered from leftmost branch sum to rightmost 
+// branch sum
