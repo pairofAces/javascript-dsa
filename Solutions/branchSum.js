f06d734d5c5e9518ab920399c2c9bb27c62b9430
pairofAces <karanchauhan16@gmail.com>
2021-01-13 23:06:00 -0500

    created BinaryTree class in branchSum.js

--- Solutions/branchSum.js
+++ Solutions/branchSum.js
@@ -7,3 +7,11 @@
 // A branch sum is a the sum of all the values in a Binary Tree Branch. 
     // A Binary Tree Branch is a a path of nodes in a tree that starts 
     // at the (root) node and ends at any (leaf) node. 
+
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
