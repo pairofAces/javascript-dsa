9f8ccfc35df3e5638961e5b90f9b125dd81886d5
pairofAces <karanchauhan16@gmail.com>
2021-01-14 23:22:14 -0500

    added question to nodeDepth.js

--- Solutions/nodeDepth.js
+++ Solutions/nodeDepth.js
@@ -2,3 +2,6 @@
 
 // Node Depth: the distance between a node in a Binary
 // Tree and the tree's root
+
+// Write a function that takes in a Binary Tree and 
+// returns the sum of its nodes' depths.
