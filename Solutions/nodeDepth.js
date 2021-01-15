aa8d70eed329e4ff67923dc1ae6ef3840da7f2f9
pairofAces <karanchauhan16@gmail.com>
2021-01-14 23:26:30 -0500

    added A.E. solution

--- Solutions/nodeDepth.js
+++ Solutions/nodeDepth.js
@@ -7,5 +7,8 @@
 // returns the sum of its nodes' depths.
 
 function nodeDepths(root, depth = 0) {
-
+    if (root === null) {
+        return 0;
+    }
+    return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1);
 };
