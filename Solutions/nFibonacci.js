ca627c4119759c6e840da0ea997c8843fca2b5ae
pairofAces <karanchauhan16@gmail.com>
2021-01-18 22:57:03 -0500

    created new question template

---
+++ Solutions/nFibonacci.js
@@ -0,0 +1,9 @@
+// Nth Fibonacci A.E. 
+
+// The Fibonacci sequence: 
+    // the first number of the sequence is 0, the second number
+    // is 1, and the (nth) number is the sum of the (n - 1)th 
+    // and (n - 2)th numbers. 
+
+    // Write a function that takes in an integer (n) and 
+    // returns the (nth) Fibonacci number. 
