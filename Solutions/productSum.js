2623bb73173070fdcadaf31cbe407a9a7d63138c
pairofAces <karanchauhan16@gmail.com>
2021-01-19 22:38:39 -0500

    time/space complexities done

--- Solutions/productSum.js
+++ Solutions/productSum.js
@@ -25,7 +25,8 @@
         // where (n) is the total number of elements in an array
 
     // Space Complexity: O(d)
-        // 
+        // where (d) is the greatest depth of "special"
+        // arrays in the array
 function productSum(array, multiplier = 1) {
     let sum = 0;
     for (const ele of array) {
