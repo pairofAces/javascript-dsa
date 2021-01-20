42bc5e424d416dac6df56239785c30b817c26bbf
pairofAces <karanchauhan16@gmail.com>
2021-01-19 22:23:48 -0500

    added depth definition for productSum.js

--- Solutions/productSum.js
+++ Solutions/productSum.js
@@ -3,3 +3,10 @@
 // create a function that takes in a "special" array
 // and returns the product sum
 
+    // The depth of a "special" array is how far
+    // nested it is.
+
+    // Example:
+        // The depth of [] = 1
+        // The depth of [[]] = 2
+        // The depth of [[[]]] = 3
