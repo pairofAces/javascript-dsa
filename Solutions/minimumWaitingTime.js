7fd99074e51755745934e5ec0081a1a641ca43b4
pairofAces <karanchauhan16@gmail.com>
2021-01-20 22:33:03 -0500

    adding minimum waiting time question

---
+++ Solutions/minimumWaitingTime.js
@@ -0,0 +1,6 @@
+// Minimum Waiting Time
+
+// Given a non-empty array of positive integers representing
+// the amount of time that queries take to execute.
+    // Only 1 query can be executed at a time
+    // queries can be executed in any order
