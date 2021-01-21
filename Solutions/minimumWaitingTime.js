46a28af309161df474fb16fe5b994e390c0133f9
pairofAces <karanchauhan16@gmail.com>
2021-01-20 22:35:49 -0500

    working on min waiting time question

--- Solutions/minimumWaitingTime.js
+++ Solutions/minimumWaitingTime.js
@@ -4,3 +4,12 @@
 // the amount of time that queries take to execute.
     // Only 1 query can be executed at a time
     // queries can be executed in any order
+
+    // A queries "waiting time" is the time a query needs to
+    // wait before starting its execution.
+        // Example:
+            // If a query is 2nd, then its wait time is the 
+            // duration of the 1st query.
+
+            // If a query is 3rd, the its wait time is the 
+            // duration of the 1st query.
