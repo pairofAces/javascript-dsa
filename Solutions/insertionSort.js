f8eb72e48a46757aa162fb29f0cd4f5bca56fc93
pairofAces <karanchauhan16@gmail.com>
2021-01-21 23:12:56 -0500

    finished removeMin for loop

--- Solutions/insertionSort.js
+++ Solutions/insertionSort.js
@@ -25,4 +25,10 @@ unsortedArray;
 function removeMin(array) {
     let min = array[0];
     let minI = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+            minI = i;
+        }
+    }
 };
