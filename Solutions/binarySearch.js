fab33ab3b261ab3e3aafcb0a68ee302a6d9b717e
pairofAces <karanchauhan16@gmail.com>
2021-01-23 00:23:29 -0500

    added psuedo instructions for binary search

--- Solutions/binarySearch.js
+++ Solutions/binarySearch.js
@@ -3,3 +3,9 @@
 // Write a function that takes an array of integers
 // and a target integer.
 
+// The function needs to be able to iterate through the
+// array and check for the target integer.
+    // If it is:
+        // return the index of the element
+    // If not:
+        // return -1
