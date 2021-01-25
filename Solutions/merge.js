6876c58c17500f61acac69afb761d6a16711ea77
pairofAces <karanchauhan16@gmail.com>
2021-01-24 23:08:36 -0500

    created 2 example arrays

--- Solutions/merge.js
+++ Solutions/merge.js
@@ -1,1 +1,7 @@
 // adding merge sorting example and solution
+
+// Starting with 2 ordered arrays, combine them into 1 ordered array
+
+let costItemsAtTJ = [1, 2, 6, 7, 8]
+let costItemsAtWF = [3, 4, 5, 9, 10]
+
