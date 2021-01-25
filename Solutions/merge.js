2c2d70b1f7450c31e1243927b249e42b98c837b7
pairofAces <karanchauhan16@gmail.com>
2021-01-24 23:11:38 -0500

    creating findMin function

--- Solutions/merge.js
+++ Solutions/merge.js
@@ -5,3 +5,9 @@
 let costItemsAtTJ = [1, 2, 6, 7, 8]
 let costItemsAtWF = [3, 4, 5, 9, 10]
 
+// Since these 2 subarrays are already ordered, the minimum of each
+// must be at the index 0 location
+
+function findMin(costItemsAtTJ, costItemsAtWF) {
+
+};
