d95ca0229dc67cdb74ba9b11ecdc6f0690ccdb3b
pairofAces <karanchauhan16@gmail.com>
2021-01-24 23:13:05 -0500

    created findMin function

--- Solutions/merge.js
+++ Solutions/merge.js
@@ -2,12 +2,19 @@
 
 // Starting with 2 ordered arrays, combine them into 1 ordered array
 
-let costItemsAtTJ = [1, 2, 6, 7, 8]
-let costItemsAtWF = [3, 4, 5, 9, 10]
+let first = [1, 2, 6, 7, 8]
+let second = [3, 4, 5, 9, 10]
 
 // Since these 2 subarrays are already ordered, the minimum of each
 // must be at the index 0 location
 
-function findMin(costItemsAtTJ, costItemsAtWF) {
+function findMin(first, second) {
+    let minFirst = first[0];
+    let minSecond = second[0];
 
+    if (minFirst < minSecond) {
+        return minFirst;
+    } else {
+        return minSecond;
+    }
 };
