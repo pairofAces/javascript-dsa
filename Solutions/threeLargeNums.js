a6b53c9611d5cdc84eeac947b1809a03781fbeaf
pairofAces <karanchauhan16@gmail.com>
2021-01-25 22:59:17 -0500

    fixed threeLargestNums.js

--- Solutions/threeLargeNums.js
+++ Solutions/threeLargeNums.js
@@ -25,11 +25,11 @@ function updateLargest(threeLargest, num) {
     if (threeLargest[2] === null || num > threeLargest[2]) {
         // another aspirational code to shift the numbers in the 
         // threeLargest array
-        shiftAndUpdate(threeLargest, num, idx) // need to work on this
+        shiftAndUpdate(threeLargest, num, 2)
     } else if (threeLargest[1] === null || num > threeLargest[1]) {
-        shiftAndUpdate(threeLargest, num, idx) // need to work on this
+        shiftAndUpdate(threeLargest, num, 1)
     } else if (threeLargest[0] === null || num > threeLargest[0]) {
-        shiftAndUpdate(threeLargest, num, idx) // need to work on this
+        shiftAndUpdate(threeLargest, num, 0)
     };
 };
 
