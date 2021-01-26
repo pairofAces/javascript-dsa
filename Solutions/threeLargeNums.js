40b38621a3c41631cc59e198b81f2f00bf12ce8e
pairofAces <karanchauhan16@gmail.com>
2021-01-25 22:29:10 -0500

    added 3 largest numbers question template

---
+++ Solutions/threeLargeNums.js
@@ -0,0 +1,8 @@
+// Find the 3 largest numbers
+
+// write a function that takes an array of at least three integers
+// and returns a sorted array of the three largest integers within
+// the array
+
+    // note -> Do not sort the input array
+
