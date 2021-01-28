a944111bc8d1304701e1be626c0d8962b4e6bcad
pairofAces <karanchauhan16@gmail.com>
2021-01-27 19:54:06 -0500

    Added sample

--- Solutions/insertionSort.js
+++ Solutions/insertionSort.js
@@ -66,3 +66,6 @@ function selectionSort(array) {
 // Write a function that takes an array of integers and returns a
 // sorted version of the array. Use the insertion sort algorithm.
 
+// sample:
+    // input: [8, 5, 2, 9, 5, 6, 3]
+    // output: [2, 3, 5, 5, 6, 8, 9]
