589ef58271a360a739443b40b847264736636244
pairofAces <karanchauhan16@gmail.com>
2021-01-28 21:57:19 -0500

    created while loop

--- Solutions/selectionSort.js
+++ Solutions/selectionSort.js
@@ -4,6 +4,14 @@
 // a sorted version. 
 
 function selectionSort(array) {
+    // create a variable for the starting index
     let startIdx = 0;
     
+    // use a while loop for while the variable above is
+    // less than the array length minus 1, since the first
+    // number is at index 0, the last has to be at the 
+    // index of (array.length - 1)
+    while (startIdx < array.length - 1) {
+        
+    }
 }
