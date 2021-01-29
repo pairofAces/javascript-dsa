6e00779c1b4b8bcb917fcf17dce826e4e71475aa
pairofAces <karanchauhan16@gmail.com>
2021-01-28 22:00:03 -0500

    working on a while loop to compare startIdx to smallestIdx

--- Solutions/selectionSort.js
+++ Solutions/selectionSort.js
@@ -12,6 +12,12 @@ function selectionSort(array) {
     // number is at index 0, the last has to be at the 
     // index of (array.length - 1)
     while (startIdx < array.length - 1) {
+        // create a variable for the 'smallest' index
+        // to be equal to the startIdx
+        let smallestIdx = startIdx;
+
+        // use a for loop to compare the startIdx and
+        // the smallestIdx. 
         
     }
 }
