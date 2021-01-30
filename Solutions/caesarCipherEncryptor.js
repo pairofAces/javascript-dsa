c8c93c2d99e9e6d4638dc56d000022eb97911b42
pairofAces <karanchauhan16@gmail.com>
2021-01-29 22:01:44 -0500

    added the question template

---
+++ Solutions/caesarCipherEncryptor.js
@@ -0,0 +1,8 @@
+// Caesar Cipher Encryptor
+
+// Given a string of lowercase letters and a non-negative integer
+// representing a key. 
+
+// Write a function that returns a new string by shifting every letter
+// in the original string by k positions in the alphabet. 
+    // k is the key
