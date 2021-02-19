23f4cfe849154b48038714beebb739bf05834152
pairofAces <karanchauhan16@gmail.com>
2021-02-19 18:09:25 -0500

    inserted function template

--- Solutions/richestCustomer.js
+++ Solutions/richestCustomer.js
@@ -9,3 +9,7 @@
     // a customers wealth is the amount of money they have in all 
     // their bank accounts. 
         // richest customer is the customer with maximum wealth
+
+const maximumWealth = function(accounts) {
+
+};
