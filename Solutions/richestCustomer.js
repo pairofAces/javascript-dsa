4166f37f2820522f8476a8fa2e57fa7a273f09c4
pairofAces <karanchauhan16@gmail.com>
2021-02-19 17:58:24 -0500

    inserted the prompt

--- Solutions/richestCustomer.js
+++ Solutions/richestCustomer.js
@@ -1,3 +1,11 @@
 // Richest customer wealth 
 
 // Prompt
+
+// Given an (m * n) integer grid accounts, where accounts[i][j] is
+// the amount of money the ith customer has in the jth bank.
+    // return the wealth that the richest customer has
+
+    // a customers wealth is the amount of money they have in all 
+    // their bank accounts. 
+        // richest customer is the customer with maximum wealth
