8fd91e299fbb759c62d8f9738a9960561a643f0a
pairofAces <karanchauhan16@gmail.com>
2021-02-24 23:07:43 -0500

    added note about function

--- Solutions/Hard/fourNumberSum.js
+++ Solutions/Hard/fourNumberSum.js
@@ -6,7 +6,8 @@
 // The function should find all quadruplets in the array
 // that sum up to the target sum and return  two-dimensional
 // array of all the quadruplets
+    // if no quadruplets -> return an empty array
 
 function fourNumberSum(array, targetSum) {
-    
+
 }
