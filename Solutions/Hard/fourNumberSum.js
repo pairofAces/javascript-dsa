e9c3de01f83de5aa66569326cd82ebe4a9c9f1e9
pairofAces <karanchauhan16@gmail.com>
2021-02-24 23:06:42 -0500

    created fourNumberSum file and prompt

---
+++ Solutions/Hard/fourNumberSum.js
@@ -0,0 +1,8 @@
+// Four Number Sum
+
+// Prompt:
+// create a function that takes an array of distinct
+// integers and an integer representing a target sum.
+// The function should find all quadruplets in the array
+// that sum up to the target sum and return  two-dimensional
+// array of all the quadruplets
