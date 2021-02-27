e84e12effcbe74d0cd9a5d3973aa05dceb687d3a
pairofAces <karanchauhan16@gmail.com>
2021-02-26 19:16:44 -0500

    added important variables for meanderingArray

--- Solutions/Medium/meanderingArray.js
+++ Solutions/Medium/meanderingArray.js
@@ -2,7 +2,12 @@
 // the function will take an unsorted array as a parameter
 
 function meanderingArray(array) {
+    // set a variable to the array's length and also sort it
     let n = array.length;
     let sorted = array.sort((a, b) => a - b);
-    
+    // create an empty array with var's for the smallest and
+    // largest number
+    let newArray = [];
+    let small = 0;
+    let large = n - 1;
 }
