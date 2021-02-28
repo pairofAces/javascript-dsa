c68733fa8db6dec831ac7c4b24f6247004ff9b7b
pairofAces <karanchauhan16@gmail.com>
2021-02-27 20:44:38 -0500

    added delete and earn prompt

---
+++ Solutions/Medium/deleteAndEarn.js
@@ -0,0 +1,9 @@
+// Delete and Earn - Leetcode
+
+// Prompt
+// Given an array of integers, you an perform operaitons
+// In each operation, pick any integer element and delete it
+// to earn points equal to that integer. 
+    // after deleting that integer, delete all equal elements
+    // equal to that integer +/- 1.
+// start with 0 points, return the max number of points possible
