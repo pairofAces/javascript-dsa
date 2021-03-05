e6a308f0d9a279dc98fe02ef4f91333d5e25ae76
pairofAces <karanchauhan16@gmail.com>
2021-03-04 21:33:06 -0500

    added smallestDifference Prompt

---
+++ Solutions/Medium/smallestDifference.js
@@ -0,0 +1,9 @@
+// SMALLEST DIFFERENCE
+// Promt 
+
+// write a function that takes in two non-empty arrays of integers
+// and finds the pair of numbers whose absolute difference is 
+// closest to zero
+    // return an array of the two numbers
+        // the number in the 1st array should be in the 1st position
+        // the number in the 2nd array should be in the 2nd position
