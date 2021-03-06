120e301b2af49b3f788ecb3a194a2b8817e97c53
pairofAces <karanchauhan16@gmail.com>
2021-03-06 18:55:17 -0500

    created variable to check the ending of the input time

--- Solutions/HR/timeConversion.js
+++ Solutions/HR/timeConversion.js
@@ -5,5 +5,8 @@
 // function will take a string as a parameter, in 12 hour format
 
 function timeConversion(s) {
-
+    // create a variable for the ending, AM or PM
+    let amPm = s.charAt(8);
+    let time = "";
+    
 };
