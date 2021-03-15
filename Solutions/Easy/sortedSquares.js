790b08ac41a361ca42bf7524adf9bbf052181204
pairofAces <karanchauhan16@gmail.com>
2021-03-14 22:07:26 -0400

    initiated a for loop to traverse the the new array from right to left

--- Solutions/Easy/sortedSquares.js
+++ Solutions/Easy/sortedSquares.js
@@ -11,5 +11,15 @@ function sortedSquares(nums) {
     let smallerPointer = 0;
     let largerPointer = nums.length - 1;
 
+    // use a for loop to iterate over the new array, from
+    // right to left... filling the last position of the 
+    // array first, then decrementing... essentialy putting
+    // the larger numbers last -> resulting in final sorted
+    // array from least to greatest
+    for (let idx = nums.length - 1; idx >= 0; idx--) {
+
+    }
     
+    // return the sorted array
+    return sortedResult;
 };
