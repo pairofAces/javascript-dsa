598bb362b60c71126afd9297b9ddbda7dfc1e3fb
pairofAces <karanchauhan16@gmail.com>
2021-03-14 22:02:26 -0400

    added sorted squares prompt

---
+++ Solutions/Easy/sortedSquares.js
@@ -0,0 +1,7 @@
+// SORTED SQUARES - LEETCODE 997
+// PROMPT
+
+// Given an integer array (nums) sorted in non-decreasing order,
+// return an array of the squares of each number sorted in
+// non-decreasing order.
+
