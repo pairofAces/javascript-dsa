a09e3aac21865f60fb13fb38b55d271e5ec14129
pairofAces <karanchauhan16@gmail.com>
2021-03-15 23:06:00 -0400

    added nonContructibleChange file and prompt

---
+++ Solutions/Easy/nonConstructibleChange.js
@@ -0,0 +1,10 @@
+// NON-CONSTRUCTIBLE CHANGE
+// PROMPT:
+
+// Given an array of positive numbers, representing the values of
+// the coins you have, create a function that returns the minimum
+// amount of change that you can't make. 
+    // the numbers in the array don't have to be unique
+    // example: array = [1, 2, 5]
+        // The minimum amount of change I can't create is 4.
+    // If there are no coins, the minimum is 1.
