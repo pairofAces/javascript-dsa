2cd29e17b8228f840bda308db46daa65c4249421
pairofAces <karanchauhan16@gmail.com>
2021-03-22 21:42:22 -0400

    added files and prompt for monotonic array

---
+++ Solutions/Medium/monotonicArray.js
@@ -0,0 +1,17 @@
+// Monotonic Array
+
+// The Prompt
+
+// write a function that takes an array of integers and returns a
+// boolean representing whether the array is monotonic.
+    // an array is monotonic if its elements are non-increasing
+    // or non-decreasing, from left-to-right
+
+    // non-increasing elements aren't just decreasing, they
+    // just don't increase.
+
+    // non-decreasing elements aren't just increasing, they
+    // just don't decrease.
+
+// Note -> empty arrays and array's of 1 element are monotonic
+
