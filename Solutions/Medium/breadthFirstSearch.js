a05b9b148e000b0d917286ac7b02a7ee56d98936
pairofAces <karanchauhan16@gmail.com>
2021-03-23 22:12:39 -0400

    creatd BFS file and inserted prompt

---
+++ Solutions/Medium/breadthFirstSearch.js
@@ -0,0 +1,12 @@
+// BREADTH FIRST SEARCH
+
+// The Prompt
+
+// Given a (Node) class that has a (name) and an array of optional
+// children nodes.
+
+// Implement the breadthFirstSearch (BFS) method on the (Node) class,
+// which will take an empty array, traverses the tree using the 
+// BFS approach (Navigating the tree from left-to-right), stores all
+// the nodes' names in the input array and then returns it.
+ 
