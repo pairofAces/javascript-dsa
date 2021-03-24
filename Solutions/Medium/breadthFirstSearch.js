3a0bdd5e302ff4338933e54b37bbb7e0c83464c9
pairofAces <karanchauhan16@gmail.com>
2021-03-23 22:13:22 -0400

    creating Node class

--- Solutions/Medium/breadthFirstSearch.js
+++ Solutions/Medium/breadthFirstSearch.js
@@ -9,4 +9,11 @@
 // which will take an empty array, traverses the tree using the 
 // BFS approach (Navigating the tree from left-to-right), stores all
 // the nodes' names in the input array and then returns it.
- 
+
+class Node {
+    constructor(name) {
+        this.name = name;
+        this.children = [];
+    }
+    
+}
