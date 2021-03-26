4d6df77f56fe36fb8febff75a5df3e054238c5dc
pairofAces <karanchauhan16@gmail.com>
2021-03-25 22:18:27 -0400

    created counting function template for apples and oranges

--- Solutions/HackerRank/appleAndOrange.js
+++ Solutions/HackerRank/appleAndOrange.js
@@ -1,4 +1,16 @@
 // Hackerrank Apple and Orange
 
+// https://www.hackerrank.com/challenges/apple-and-orange/problem
 
+// parameters:
+    // s: integer, starting point of Sam's house location.
+    // t: integer, ending location of Sam's house location.
+    // a: integer, location of the Apple tree.
+    // b: integer, location of the Orange tree.
+    // apples: integer array, distances at which each apple falls from the tree.
+    // oranges: integer array, distances at which each orange falls from the tree.
+
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+
+};
 
