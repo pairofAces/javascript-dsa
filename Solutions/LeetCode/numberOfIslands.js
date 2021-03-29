bc96c4805d3bf5da56e4c802fc9830533b8d42c1
pairofAces <karanchauhan16@gmail.com>
2021-03-28 21:36:16 -0400

    working on leetcode -> number of islands

---
+++ Solutions/LeetCode/numberOfIslands.js
@@ -0,0 +1,10 @@
+// Number Of Islands
+
+// Prompt
+// Given an (m X n) 2D binary grid which represents a map of '1's (land)
+// and '0's (water), return the number of islands.
+
+    // An Island is surrounded by water and is formed by connecting
+    // adjacent lands horizontally or vertically.
+        // Assume all four edges of the grid are all surrounded by 
+        // water
