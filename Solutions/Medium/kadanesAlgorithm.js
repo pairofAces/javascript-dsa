6826e440d78440faf5a3bd61f33bc0d198fb2ca8
pairofAces <karanchauhan16@gmail.com>
2021-04-01 22:50:35 -0400

    created file and included prompt for kadane's algorithm

---
+++ Solutions/Medium/kadanesAlgorithm.js
@@ -0,0 +1,7 @@
+// KADANES ALGORITHM
+
+// PROMPT
+// Create a function that takes in a non-empty array of integers and
+// returns the maxmum sum that can be obtained by summing up all of the
+// integers in a non-empty subarray of the input array. 
+    // a subarray must only contain adjacent numbers
