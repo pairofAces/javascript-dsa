11fb8b29f974b140a775950364270f3932a4db35
pairofAces <karanchauhan16@gmail.com>
2021-04-02 23:36:54 -0400

    created function template

--- Solutions/Medium/maxSubsetSumNoAdjacent.js
+++ Solutions/Medium/maxSubsetSumNoAdjacent.js
@@ -4,3 +4,6 @@
 // Create a function that takes an array of positive integers
 // and returns the max sum of non-adjacent elements in the array.
     // if input array is empty, function should return 0
+function maxSubsetSumNoAdjacent(array) {
+    
+}
