64334726c100e4920a894af11fe9ae82e220deba
pairofAces <karanchauhan16@gmail.com>
2021-04-02 23:37:57 -0400

    working on conditional logic

--- Solutions/Medium/maxSubsetSumNoAdjacent.js
+++ Solutions/Medium/maxSubsetSumNoAdjacent.js
@@ -5,5 +5,9 @@
 // and returns the max sum of non-adjacent elements in the array.
     // if input array is empty, function should return 0
 function maxSubsetSumNoAdjacent(array) {
-    
+    // use conditional logic
+        // if the array is empty, return 0
+    if (!array.length) {
+        return 0;
+    }
 }
