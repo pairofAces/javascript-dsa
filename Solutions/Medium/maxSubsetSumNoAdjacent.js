6563af6939f524e4b6d35fdff2f9f563f9fde256
pairofAces <karanchauhan16@gmail.com>
2021-04-02 23:36:20 -0400

    created maxSubsetSumNoAdjacent.js file and included prompt

---
+++ Solutions/Medium/maxSubsetSumNoAdjacent.js
@@ -0,0 +1,6 @@
+// MAX SUBSET SUM NO ADJACENT
+
+// PROMPT
+// Create a function that takes an array of positive integers
+// and returns the max sum of non-adjacent elements in the array.
+    // if input array is empty, function should return 0
