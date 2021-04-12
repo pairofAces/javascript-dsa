c46d41e268908c541f952e053ec76a3a49febcfa
pairofAces <karanchauhan16@gmail.com>
2021-04-11 21:44:38 -0400

    created file name and prompt for three largest numbers

---
+++ Solutions/Easy/threeLargestNums.js
@@ -0,0 +1,14 @@
+// FIND THREE LARGEST NUMBERS
+
+// PROMPT
+// Create a function that takes an array and returns a
+// sorted array of the three largest integers in the 
+// input array.
+    // Don't sort the input array
+    // if there are duplicate integers in the input array,
+    // the resulting array should contain those integers
+    // if they are the largest.
+
+function findThreeLargestNumbers(array) {
+
+};
