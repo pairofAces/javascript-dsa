83e68f1ffa4ed4d85b4d04ad4295a9616181d2b5
pairofAces <karanchauhan16@gmail.com>
2021-04-14 23:30:49 -0400

    created question prompt and function template

---
+++ Solutions/Medium/powerset.js
@@ -0,0 +1,12 @@
+// Powerset
+
+// PROMPT
+
+// create a function that takes an array of unique nubers and returns
+// its powerset. 
+    // the powerset of P(X) of a set X, is the set of all subsets of X
+        // example:
+            // powerset of [1,2] is [[], [1], [2], [1,2]]
+function powerset(array) {
+    
+}
