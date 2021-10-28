93f0bfc0ed5b1694c9a69041c9d4d4225b8c8b37
pairofAces <karanchauhan16@gmail.com>
2021-10-27 22:54:03 -0400

    created solution for longest common prefix

---
+++ Solutions/Easy/longestCommonPrefix.js
@@ -0,0 +1,11 @@
+// Leetcode 14 - Longest Common Prefix
+
+// create a function to find the longest common prefix from an
+// array of strings
+
+// note: if there's no common prefix, return an empty string
+
+var longestCommonPrefix = function (strs) {
+    let prefix = '';
+    
+}
