47e011eb72173355577fa0033825ed0c7122bfc0
pairofAces <karanchauhan16@gmail.com>
2021-10-29 23:54:58 -0400

    creating traversal logic

--- Solutions/Easy/romanToInt.js
+++ Solutions/Easy/romanToInt.js
@@ -1,5 +1,20 @@
 // Roman to integer, leetcode 13
 
 var romanToInt = function(s) {
-    
+    // create a dictionary for reference between roman numerals and numbers
+    const legend = {
+        I:1,
+        v:5,
+        x:10, 
+        L:50,
+        C:100,
+        D:500,
+        M:1000
+    }
+
+    // create variable to reference the total
+        // initated to 0
+    let total = 0;
+
+    // logic
 }
