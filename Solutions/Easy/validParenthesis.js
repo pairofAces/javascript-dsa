76c2a15c28f0bd4ae510110ffb5b3d0f18d740d8
pairofAces <karanchauhan16@gmail.com>
2021-10-31 23:24:36 -0400

    created template for leetcode 20

---
+++ Solutions/Easy/validParenthesis.js
@@ -0,0 +1,15 @@
+// Leetcode 20. Valid Parenthesis
+
+// Given a string s containing just the characters 
+// '(', ')', '{', '}', '[' and ']', 
+// determine if the input string is valid.
+
+// Note:
+    // An input string is valid if:
+        // Open brackets must be closed by the same type of brackets.
+        // Open brackets must be closed in the correct order.
+
+
+var isValid = function(s) {
+
+}
