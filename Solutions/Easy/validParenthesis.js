53c4a4d6893d04ab820463c367ed3a097c5cf281
pairofAces <karanchauhan16@gmail.com>
2021-11-01 23:20:42 -0400

    created solution for valid parenthesis

--- Solutions/Easy/validParenthesis.js
+++ Solutions/Easy/validParenthesis.js
@@ -12,4 +12,30 @@
 
 var isValid = function(s) {
 
+    var stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        let ch = charAt(i);
+        
+        if (ch === "(" || ch === "[" || ch === "{") {
+            // push the correct closing bracket into (stack)
+                // use a helper function to take care of the logic
+            stack.push(getClosingBracket(ch));
+        } else if (stack.length === 0 || stack.pop() !== ch) {
+            return false;
+        }
+    }
+    return stack.length === 0;
+}
+
+// create a helper function (getClosingBracket)
+    // take the (ch) variable as an input
+function getClosingBracket(ch) {
+    if(ch === "(") {
+        return ")";
+    } else if (ch === "[") {
+        return "]";
+    } else if (ch === "{") {
+        return "}";
+    }
 }
