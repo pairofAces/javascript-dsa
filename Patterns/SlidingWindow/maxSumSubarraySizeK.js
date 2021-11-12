410853d2cd0b1c50183ce9dd390ce8b8508ee042
pairofAces <karanchauhan16@gmail.com>
2021-11-11 21:15:35 -0500

    created template for finding max sum of subarray

---
+++ Patterns/SlidingWindow/maxSumSubarraySizeK.js
@@ -0,0 +1,6 @@
+// Given an array of positive integers and a positive number 'K',
+// find the maximum sum of any contiguous subarray of size 'K'
+
+const max_sum_subarray_of_size_k = (array, k) => {
+    
+}
